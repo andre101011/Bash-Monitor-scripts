b9a8fde1be175e13cc33c0502831b17abbacbf22
Neyder Figueroa <nfigueroas@uqvirtual.edu.co>
2020-11-08 13:34:15 -0500

    Desarrollo del punto 2 al 95%

--- ramState.sh
+++ ramState.sh
@@ -2,50 +2,61 @@
 
 #Elaborado por Neyder Figueroa,Andres Llinas, Daniel Bonilla
 
+#TODO:  DESARROLLO AL 95%, falta cuadrar el icono y revisar el tema de los errores
+
+export DISPLAY=:0
 
 
-#totalMemory=$(top|head -4| tail -1 | cut -d',' -f1 | awk '{print $4}') Opcion alterna
+if [ ! -f .ramStateLog ] #se crea el log
+    then
+        touch ramState.log
+fi
+
+
+dateLog=$(date)
+
 totalMemory=$(free | head -2 |tail -1| awk '{print $2}')
 
-#usedMemory=$(top|head -4| tail -1 | cut -d',' -f3 |awk '{print $2}' Opcion alterna
 usedMemory=$(free | head -2 |tail -1| awk '{print $3}')
+
+usedMemoryPercentage=$(echo "scale=2; ($usedMemory*100)/$totalMemory" | bc) #calcula el porcentaje de uso
+
+processProblem=$(top -n1 -b | head -8| tail -1| awk '{print $12}') #arroja el processo que esta consumiendo mas memoria
 
 
 echo "-> memoria total en KB ${totalMemory}"
 echo "-> memoria usada en KB ${usedMemory}"
-
-#usedMemoryPercentage=$((($usedMemory *100)/$totalMemory))
-usedMemoryPercentage=$(echo "scale=2; ($usedMemory*100)/$totalMemory" | bc) #calcula el porcentaje de uso
-
-
 echo "-> porcentaje de memoria usada: $usedMemoryPercentage %"
-#6-->100%
-#used--> X
-
-processProblem=$(top | head -8| tail -1|  awk '{print $13}') #arroja el processo que esta consumiendo mas memoria
+echo "-> proceso que mas consume: $processProblem "
 
 
 condition0=$(echo "$usedMemoryPercentage > 75.00" | bc) #porcentaje para el caso 1
 condition1=$(echo "$usedMemoryPercentage > 20.00" | bc) #porcentaje para el caso 2
-condition2=$(echo "$usedMemoryPercentage < 25.38" | bc) #porcetaje para el caso 2
+condition2=$(echo "$usedMemoryPercentage < 22.38" | bc) #porcetaje para el caso 2
 
+alert="No aplica"
 
-    if [[ $condition2 -eq 1 && $condition1 -eq 1 ]]
+if [[ $condition2 -eq 1 && $condition1 -eq 1 ]]
     then
-        notify-send --icon=message_box_info "ALTO CONSUMO DE MEMORIA, CUIDADO CON EL PROCESO $processProblem" #buscar lo adecuado
+        notify-send --icon=message_box_info -t 5000000 "ALERTA ALTO CONSUMO DE MEMORIA" "\n$usedMemoryPercentage% alcanzado"  #buscar lo adecuado
+        alert="caso 2"
     else
         if [ $condition0 -eq 1 ]
         then
-            notify-send --icon=message_box_info "ALTO CONSUMO DE MEMORIA, $usedMemoryPercentage alcanzado" #buscar lo adecuado
-           
+            notify-send --icon=message_box_info -t 5000000 "ALERTA ALTO CONSUMO DE MEMORIA" "\nCUIDADO CON EL PROCESO $processProblem " #buscar lo adecuado
+            alert="caso 1"
         fi
-    fi
+    
+fi
 
-    if [ -f .ramStateLog  ] #se actualiza o se crea el log
-    then
-        echo "lo que sea que vaya acá">>.ramStateLog
-    else
-        touch .ramStateLog
-    fi
 
+if [ $condition0 -eq 1 ] 
+then
+    echo "_Memoria en uso: $usedMemoryPercentage% Tipo caso: $alert  Fecha: $dateLog Proceso: $processProblem ">>ramState.log
+else
+    echo "_Memoria en uso: $usedMemoryPercentage% Tipo caso: $alert  Fecha: $dateLog">>ramState.log
+
+fi
+
+    
 
